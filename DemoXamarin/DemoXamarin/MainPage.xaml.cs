2381eda1a5d963a1a1986a57ea4b18cb24a62946
Allan Pead <adpead@gmail.com>
2022-05-04 15:22:47 +0200

    first commit

---
+++ DemoXamarin/DemoXamarin/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        private int _numberClicked;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CoolSlider.ValueChanged += OnSliderValueChanged;
+        }
+
+        private void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            ClickCountLabel.Text = e.NewValue.ToString();
+
+            ClickCountLabel.Rotation = e.NewValue;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            _numberClicked++;
+
+            ClickCountLabel.Text = _numberClicked.ToString(); 
+
+        }
+
+        private void Xappiness_Clicked(object sender, EventArgs e)
+        {
+            // show xappiness
+
+            Navigation.PushAsync(new XappinessPage());
+        }
+    }
+}
---
+++ DemoXamarin/DemoXamarin/XappinessPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class XappinessPage : ContentPage
+    {
+        public XappinessPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
